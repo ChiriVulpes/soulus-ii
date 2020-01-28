ab16b3de9676bf0966efc6d6f30b3f672bcfeb1a
Mackenzie McClane <yuudaari@gmail.com>
2020-01-28 15:01:45 +1100

    Initial commit

---
+++ .vscode/debug.js
@@ -0,0 +1,12 @@
+const childProcess = require("child_process");
+const fs = require("fs");
+
+childProcess.exec(`@echo off & start /WAIT debug.bat ${process.argv.includes("--server") ? "runServer" : "runClient"}`);
+
+fs.writeFileSync("compile_log.txt", "");
+
+setInterval(() => {
+	const fileText = fs.readFileSync("compile_log.txt", "utf16le");
+	if (fileText.includes("Listening for transport dt_socket at address"))
+		process.exit();
+}, 200);
---
+++ src/main/java/chirichirichiri/soulus/ii/SoulusII.java
@@ -0,0 +1,13 @@
+package chirichirichiri.soulus.ii;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("soulus-ii")
+public class SoulusII {
+
+	public static final Logger LOGGER = LogManager.getLogger();
+
+}
